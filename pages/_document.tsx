4a06ff3ef4ea204b2c7a10ececc719ea538e9696
Dom <dg3rmano@gmail.com>
2020-06-21 17:52:14 +1000

    Initialising Nextjs app with styled components

---
+++ pages/_document.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+
+import Document, {
+  DocumentContext,
+  Head,
+  Main,
+  NextScript
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+/**
+ * Custom document used to augment the application <html> and <body> tags.
+ *
+ *  1. Injects server side rendered styled-components in a <style> tag in the
+ *     <head>
+ *
+ * Example code here:
+ * https://github.com/zeit/next.js/blob/canary/examples/with-styled-components/pages/_document.js
+ */
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <html>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import styled from "styled-components";
+
+const Index = () => <Hello>Hello Jerry</Hello>;
+
+const Hello = styled.div`
+  color: red;
+`;
+
+export default Index;
